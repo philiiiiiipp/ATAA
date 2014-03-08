335d44cdb4facf12af86f56be05e015142f62828
tvkanters <tvkanters@gmail.com>
2014-03-08 16:37:19 +0100

    Added documentation.

--- agent/src/nl/uva/ataa/environment/fitness/FitnessFunction.java
+++ agent/src/nl/uva/ataa/environment/fitness/FitnessFunction.java
@@ -2,7 +2,13 @@ package nl.uva.ataa.environment.fitness;
 
 import nl.uva.ataa.environment.Predictor;
 
+/**
+ * A fitness function to evaluate predictor performance with.
+ */
 public interface FitnessFunction {
 
+    /**
+     * @return The fitness of the environment based on the last series of tests it has performed.
+     */
     double getFitness(Predictor predictor);
 }
